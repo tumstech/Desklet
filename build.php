d7b1b09e5c83a8983980740faa1fd6e31fb3abd1
Namo <namo@worldclasstup.com>
2013-01-24 15:25:30 +0700

    Update build.php

--- build.php
+++ build.php
@@ -6,12 +6,26 @@ $system_codename="Skycloud";
 $system_version="0.0.1";
 $system_humanreadibleversion="0.1 Alpha";
 $system_buildversion="127";
+$system_servercode="oa";
+
+
+/* Development mode hardswitches. Use this for dev purposes only! */
 $system_devmodeenabled="1";
 $system_buildisalpha="1";
 $system_buildisbeta="0";
 $system_buildispreview="0";
 $system_debugmodeenabled="0";
+
+/* ================================================ */
+
+/*
+OS Debug Values. They are used for dev purposes only. 
+It is reccommended to turn these features off in production environment. 
+*/
+
 $OSDebug_debugmodeenabled="0";
+$OSDebug_enablekernelterminal="0";
+$OSDebug_enablephpinfowindow="0";
 
 
 ?>
