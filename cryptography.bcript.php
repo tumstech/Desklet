b7641bb8aa6411861c129e4bb8d5a456bab569f6
Namo <namo@worldclasstup.com>
2013-01-23 23:32:58 -0800

    BasicCRYPT pushed

---
+++ cryptography.bcript.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Basic Encryption using built in keys. Sorry, but this code needs to be encrypted for system security. */
+
+
+eval(gzinflate(base64_decode('1VJdT8IwFH0n4T80ZCStjLgRsyBQkY89YAQMURM/sKlbkcnYsBskaPzvXjY21CkPvrmly3ruubf3np7cZOlZoeN7aL4OXlzGRcBsYcn1IsTKirsE5XNvsJTZmhamx/yo54+fh/y2ak6q7UI9n0PwKDMB0Xtt/1uoo5g+8SVWONXqCm8EoXSFh6E8gW2pRLacuOidwou6MabWVGJf2vgTRtADiqENMiYkqa7MfVsgivqd0c3FJesPuyYzO+00LDxrFx31zgbdlnnO9Eo1ZcD8wDidRyrs1IBENeoKvqCMGp+kpkRLCh4K5qxwCj2JEPYscF5Fkr/JIWpyfte8Zlej1qA77COC0hmkCJfSQzKUzhxv+lExjqYNlo+gWAwl0sE/KeuqTsgJ1RD37AwTZakNqhukiRJh0f7CpOYtXTfW+B1WPveDbWC+/24busc0dJ9lYGQKPbEFt3FsDqwbB3ji+tB00mykyyEC4UtfsSJglGrNSm2jtRrdim6UsyRCvltka7RU+99MmvXoHyy6M8AH')));
+?>
