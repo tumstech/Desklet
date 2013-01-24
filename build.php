12b60122957946800005edc161d161446e7eec18
Seth Ceriano <seth.i@me.com>
2013-01-24 11:23:25 +0700

    Added base values

--- build.php
+++ build.php
@@ -2,4 +2,16 @@
 
 /* Build Information file for Desklet */
 
+$system_codename="Skycloud";
+$system_version="0.0.1";
+$system_humanreadibleversion="0.1 Alpha";
+$system_buildversion="127";
+$system_devmodeenabled="1";
+$system_buildisalpha="1";
+$system_buildisbeta="0";
+$system_buildispreview="0";
+$system_debugmodeenabled="0";
+$OSDebug_debugmodeenabled="0";
+
+
 ?>
