104a58542d177eba7f280b0b1d22c620be12aec1
Namo <namo@worldclasstup.com>
2013-01-24 15:34:41 +0700

    Updated code, Consolidated build status string

--- build.php
+++ build.php
@@ -2,18 +2,16 @@
 
 /* Build Information file for Desklet */
 
-$system_codename="Skycloud";
-$system_version="0.0.1";
-$system_humanreadibleversion="0.1 Alpha";
-$system_buildversion="127";
-$system_servercode="oa";
-
+$system_codename="Skycloud"; // Release codename
+$system_version="0.0.1"; // System Version used for system identification
+$system_humanreadibleversion="0.1 Alpha"; // Version in the about box
+$system_buildversion="127"; // Build Version
+$system_servercode="183774773009173784638100937"; // Code for the server
+$system_uvid="00001-00011-32100-00127"; // Universal Version IDentifier
 
 /* Development mode hardswitches. Use this for dev purposes only! */
-$system_devmodeenabled="1";
-$system_buildisalpha="1";
-$system_buildisbeta="0";
-$system_buildispreview="0";
+$system_devmodeenabled="1"; // Is this still in development? turn on dev mode!
+$system_buildstatus="1"; // Build Code Status (0 nightly, 1 alpha, 2 beta, 3 preview, 4 release)
 $system_debugmodeenabled="0";
 
 /* ================================================ */
