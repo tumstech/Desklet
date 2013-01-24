a2315249200cbbf3f201bd4d2356342327286355
Seth Ceriano <seth.i@me.com>
2013-01-23 20:29:37 -0800

    Created Keybox Service File

---
+++ desktop.keybox.php
@@ -0,0 +1,16 @@
+<?php
+
+/* KeyBox Authorization and KeyBox services */
+/* 
+
+What is KeyBox?
+
+KeyBox is a PHP Script used for encryption, decryption, key management and certificate management services.
+
+*/
+
+$keybox_version="1.0";
+$encryptionkey="a97$Ks0]ji0pl7x783BxAc]pKlE8SoP0o]Na3";
+
+
+?>
