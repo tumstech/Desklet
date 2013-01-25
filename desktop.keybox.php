a666bf10248874cd659becbe21e44911621da2f6
Namo <namo@worldclasstup.com>
2013-01-25 13:22:20 +0700

    Added more encryption keys

--- desktop.keybox.php
+++ desktop.keybox.php
@@ -14,10 +14,15 @@ $keybox_version="1.0";
 /*
 encryption key
 encryption key is used to encrypt system-level variables that needs protection.
-change this if you want to run your own version of desklet. (32 Characters)
+change this if you want to run your own version of desklet. 
 */
 
-$encryptionkey="1aO8$sFhuJio08Az$]ik[wdDmOaLi8aE";
+$encryptionkey="1aO8$sFhuJio08Az$]ik[wdDmOaLi8aE"; // a default 32-bit (32 character) encryption key
+$encryptionkey_hash="f17a6adb8453f63607352b365a312249"; // a MD5 hash of the key above, for verification.
 
+$keybox_encryptionkey_16="xiU85R0{d5139l;Y"; // 16 - Bit (16 Character) Encryption Key (alphanumeric with special characters)
+$keybox_encryptionkey_numeric_16="2594103574298561"; // a 16-bit numeric-only key (not much secure)
+$keybox_encryptionkey_lowercase_16="a2c5r4g7u6v95d10"; // a 16-bit alphanumeric key, lower case only
+$keybox_encryptionkey_uppercase_16="A2E51F5Q9Z5R30OP"; // a 16-bit alphanumeric key, uppercase only
 
 ?>
