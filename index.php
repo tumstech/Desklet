c395b3a93684ce475968481bbbccc46c8658f452
Namo <seth@Apple-iMac.local>
2013-01-26 17:34:08 +0700

    Added Temporary Login page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>System Login</title>
+</head>
+
+<body>
+
+
+<form name="form1" method="post" action="auth_inc.php">
+<font face="MS Sans Serif">Username :</font> <input name="AuthorizationService_usr" type="text" id="AuthorizationService_usr" value="" size="20"><br>
+<font face="MS Sans Serif">Password :</font> <input name="AuthorizationService_pwd" type="password" id="AuthorizationService_pwd" size="20">
+<br>
+<font face="MS Sans Serif" size="2">Remember Me</font> <input name="AuthorizationService_rm" type="checkbox" id="AuthorizationService_rm" value="1">
+<br>
+<input name="AuthorizationService_Submit" type="submit" id="AuthorizationService_Submit" value="Login">
+<br>
+
+</form>
+
+</body>
+</html>
