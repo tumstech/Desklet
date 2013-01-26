e3babd78c19355f1e3138e91489de8e0ebf0e7cf
Namo <seth@Apple-iMac.local>
2013-01-26 17:49:35 +0700

    Added login page visual styling

--- index.php
+++ index.php
@@ -3,21 +3,27 @@
 <head>
 <meta charset="UTF-8">
 <title>System Login</title>
+<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 
 <body>
+ <div class="container">
+    <div class="login">
+      <h1>Desktop Login</h1>
+      <form name="form1" method="post" action="auth_inc.php">
+        <p><input name="AuthorizationService_usr" type="text" id="AuthorizationService_usr" value="" size="20" placeholder="Username"></p>
+        <p><input name="AuthorizationService_pwd" type="password" id="AuthorizationService_pwd" size="20" placeholder="Password"></p>
+        <p class="remember_me">
+          <label>
+            <input name="AuthorizationService_rm" type="checkbox" id="AuthorizationService_rm" value="1">
+          Remember me</label>
+        </p>
+        <p class="submit"><input name="AuthorizationService_Submit" type="submit" id="AuthorizationService_Submit" value="Login"></p>
+      </form>
+    </div>
+  </div>
 
 
-<form name="form1" method="post" action="auth_inc.php">
-<font face="MS Sans Serif">Username :</font> <input name="AuthorizationService_usr" type="text" id="AuthorizationService_usr" value="" size="20"><br>
-<font face="MS Sans Serif">Password :</font> <input name="AuthorizationService_pwd" type="password" id="AuthorizationService_pwd" size="20">
-<br>
-<font face="MS Sans Serif" size="2">Remember Me</font> <input name="AuthorizationService_rm" type="checkbox" id="AuthorizationService_rm" value="1">
-<br>
-<input name="AuthorizationService_Submit" type="submit" id="AuthorizationService_Submit" value="Login">
-<br>
-
-</form>
 
 </body>
 </html>
