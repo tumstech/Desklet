07088d432aa5bdc4e1d415205ea25d6dc16aedc6
Namo <seth@Apple-iMac.local>
2013-01-30 20:40:41 +0700

    Added WebKit speech input

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="login">
       <h1>Login</h1>
       <form name="form1" method="post" action="auth_inc.php">
-        <p><input name="AuthorizationService_usr" type="text" id="AuthorizationService_usr" value="" size="20" placeholder="Username"></p>
+        <p><input name="AuthorizationService_usr" type="text" id="AuthorizationService_usr" value="" x-webkit-speech size="20" placeholder="Username"></p>
         <p><input name="AuthorizationService_pwd" type="password" id="AuthorizationService_pwd" size="20" placeholder="Password"></p>
         <p class="remember_me">
           <label>
